9243d4ef262003da246f8299272f00ffbf804897
Gene <calixtogenesiscarlo@gmail.com>
2026-09-22 00:51:24 +0800

    Add user registration

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
+    path('accounts/register/', views.register, name='register'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
 
 def home(request):
     return render(request, 'tracker/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
